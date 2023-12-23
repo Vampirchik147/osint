ba9b63aec453d5b8404f729f8e6cc0590ecf79be
Levina Svetlana <36290990+Vampirchik147@users.noreply.github.com>
2023-12-23 13:09:33 +0300

    Add files via upload

---
+++ py.py
@@ -0,0 +1,19 @@
+from struct import *
+
+FILEIN = 'text.txt'
+FILEOUT = 'code_text.txt'
+KEY = 113
+
+f1 = open(FILEIN, 'rb')
+f2 = open(FILEOUT, 'wb')
+
+b_st = f1.read()
+
+for x in b_st:
+    x = int(x)
+    y = (x + KEY) % 255
+    b_y = pack('B', y)
+    f2.write(b_y)
+
+f1.close()
+f2.close()
